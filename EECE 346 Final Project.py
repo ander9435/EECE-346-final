d808daee1c8b8d658c6b47014d3d124e99d2c3fe
ander9435 <ander943@wwu.edu>
2025-06-03 09:35:49 -0700

    Mostly there Needs graphing and a couple more equations

--- EECE 346 Final Project.py
+++ EECE 346 Final Project.py
@@ -14,21 +14,21 @@ Samples = []
 nValue = 0
 xBar = 0.0
 Deviation = 0.0 
+keys = ["C90", "C95", "C99"]
 CritValue = {"C90": 0.0, "C95": 0.0, "C99": 0.0}
 StandError = 0.0
-MarginError = {"M90": 0.0, "M95": 0.0, "M99": 0.0}
-
-tTable = {"T90": [
+MarginError = {"C90": 0.0, "C95": 0.0, "C99": 0.0}
+tTable = {"C90": [
     6.314, 2.920, 2.353, 2.132, 2.015, 1.943, 1.895, 1.860, 1.833, 1.812,
     1.796, 1.782, 1.771, 1.761, 1.753, 1.746, 1.740, 1.734, 1.729, 1.725, 
     1.721, 1.717, 1.714, 1.711, 1.708, 1.706, 1.703, 1.701, 1.699, 1.697
     ],
-    "T95": [
+    "C95": [
     12.706, 4.303, 3.182, 2.776, 2.571, 2.447, 2.365, 2.306, 2.262, 2.228,
     2.201, 2.179, 2.160, 2.145, 2.131, 2.120, 2.110, 2.101, 2.093, 2.086,
     2.080, 2.074, 2.069, 2.064, 2.060, 2.056, 2.052, 2.048, 2.045, 2.042
     ],
-    "T99": [
+    "C99": [
     63.657, 9.925, 5.841, 4.604, 4.032, 3.707, 3.499, 3.355, 3.250, 3.169,
     3.106, 3.055, 3.012, 2.977, 2.947, 2.921, 2.898, 2.878, 2.861, 2.845, 
     2.831, 2.819, 2.807, 2.797, 2.787, 2.779, 2.771, 2.763, 2.756, 2.750
@@ -39,15 +39,22 @@ def User_Values():
     global Samples
     global nValue
 
-    xValues = input("Enter the sample values, you can also enter just the sample mean for calculation: ")
+    while(True):
+        xValues = input("Enter the sample values: ")
 
-    xValues = xValues.split(",")
-    
-    for i in xValues:
-        Samples.append(float(i))
+        xValues = xValues.split(",")
+        
+        for i in xValues:
+            Samples.append(float(i))
 
-    for i in Samples:
-        nValue += 1
+        for i in Samples:
+            nValue += 1
+        
+        if nValue > 1:
+            break
+
+        else:
+            print("Sample size must be 2 or more")
 
     return Samples, nValue 
 
@@ -88,15 +95,9 @@ while True:
     else:
         Choice = input("Incorrect choice, please enter an 'S' for your own data or a 'R' for an randomly generated example: ")
 
-if nValue == 1:
-    xBar = Samples
-
-else:
-    Sum = np.sum(Samples)
-    xBar = Sum/nValue
-    xBar = round(xBar, 4)
-
-print(f"{Samples}, {nValue}, {xBar}")
+Sum = np.sum(Samples)
+xBar = Sum/nValue
+xBar = round(xBar, 4)
 
 # Sample standard deviation {Samples and xBar}
 for i in Samples:
@@ -105,7 +106,8 @@ for i in Samples:
 Deviation = math.sqrt(Deviation)
 
 # Critical value {nValue, tTable_90, tTable_95, and tTable_99}
-
+for i in keys:
+    CritValue[i] = tTable[i][nValue - 2]
 
 # Standard error of the mean {Sample standard deviation and nValue}
 
@@ -114,3 +116,5 @@ Deviation = math.sqrt(Deviation)
 
 
 # Confidence intervals {xBar and Magin of errors} 
+
+print(f"{Samples}, {nValue}, {xBar}, {Deviation}, {CritValue}, ")
